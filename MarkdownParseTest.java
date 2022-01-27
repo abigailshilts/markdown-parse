1728d46205b18281447d25df33fe4e633aa7a58b
abigailshilts <abbi@shilts.net>
2022-01-26 18:59:03 -0800

    test-file3 causes index OOB error

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -17,4 +17,28 @@ public class MarkdownParseTest {
         String contents = Files.readString(fileName);
         assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
     }
+
+    @Test public void testMarkDownParseImage() throws IOException{
+        Path fileName = Path.of("/Users/abigailshilts/Documents/GitHub/markdown-parse/imageTest.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test public void testMarkDownParseNewLine() throws IOException{
+        Path fileName = Path.of("/Users/abigailshilts/Documents/GitHub/markdown-parse/imageTest.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    }
+    
+    @Test public void testMarkDownParseFile2() throws IOException{
+        Path fileName = Path.of("/Users/abigailshilts/Documents/GitHub/markdown-parse/test-file2.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
+    }
+
+    @Test public void testMarkDownParseFile3() throws IOException{
+        Path fileName = Path.of("/Users/abigailshilts/Documents/GitHub/markdown-parse/test-file3.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of(), MarkdownParse.getLinks(contents));
+    }
 }
