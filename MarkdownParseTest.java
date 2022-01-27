acdba3e84ab4c8263cd5690872d542efd15a9023
abigailshilts <abbi@shilts.net>
2022-01-26 16:48:16 -0800

    Adds JUnit test for original testFile
    Creates JUnit test to test test-file.md

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,20 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test public void testMarkDownParse() throws IOException{
+        Path fileName = Path.of("/Users/abigailshilts/Documents/GitHub/markdown-parse/test-file.md");
+        String contents = Files.readString(fileName);
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contents));
     }
 }
